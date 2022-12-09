8895bda2e3d36c96ff1587c6757fc7e836d27a1e
Avikumar Talaviya <avikumar.talaviya@gmail.com>
2022-12-09 22:03:24 +0530

    App UI and images

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 import pandas as pd
 import numpy as np
+import streamlit as st
+import joblib
+
+st.set_page_config(
+    page_title="Site Energy Intensity Prediction",
+    page_icon="⚡",
+    layout="centered",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
 
 # feature list for prediction
 feature_list = ['floor_area', 'energy_star_rating', 'ELEVATION', 'cooling_degree_days',
@@ -8,3 +21,30 @@ feature_list = ['floor_area', 'energy_star_rating', 'ELEVATION', 'cooling_degree
     'Avg_min_temp_winter', 'Avg_max_temp_winter', 'Avg_temp_winter',
     'Avg_min_temp_summer', 'Avg_max_temp_summer', 'Avg_temp_summer',
     'Avg_days_below30F']
+
+model = joblib.load("Site-Energy-Intensity-Prediction-Project\Models\eui_model.joblib")
+
+# UI for single and batch prediction using streamlit expander
+
+def single_prediction():
+    pass
+
+def batch_prediction():
+    pass
+
+def single_explainer():
+    pass
+
+def batch_explainer():
+    # summary plot
+    # decision plot
+    pass
+
+def display_shap_from_batch():
+    pass
+
+def main():
+    # main inputs
+    # model inference
+    # prediction UI
+    pass
